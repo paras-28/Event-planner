31a431ea384c1b59d069ea0d5e9221b561c1b1dc
Paras <parasarora1224@gmial.com>
2024-11-29 02:49:30 +0530

    remove unsued comment

--- lib/presentation/common_widgets/custom_appbar.dart
+++ lib/presentation/common_widgets/custom_appbar.dart
@@ -4,57 +4,7 @@ import 'package:flutter/services.dart';
 
 import '../../core/utils/app_colors.dart';
 
-/*AppBar customAppBar({
-  required String title,
-  String? keyName,
-  Color titleColor = Colors.white,
-  bool hasArrow = true,
-  VoidCallback? onBackPressed,
-  Widget? titleWidget,
-  IconData backArrowIcon = Icons.arrow_back_ios,
-  Color backArrowColor = Colors.white,
-  Color toolbarBackgroundColor = AppColors.primaryColor,
-  List<Widget> actions = const [],
-  String? fontFamily,
-  bool centerTitle = true,
-  PreferredSizeWidget? bottomWidget,
-}) {
-  return AppBar(
-    systemOverlayStyle: const SystemUiOverlayStyle(
-      statusBarColor: AppColors.primaryColor,
-      // You can use this as well
-      statusBarIconBrightness: Brightness.light,
-      // OR Vice Versa for ThemeMode.dark
-      statusBarBrightness: Brightness.light,
-    ),
-    leading: hasArrow
-        ? IconButton(
-            key: stringHasValue(keyName) ? Key(keyName!) : const Key("key"),
-            onPressed: onBackPressed ??
-                () {
-                  // Navigator.pop(context);
-                },
-            icon: Icon(
-              backArrowIcon,
-              color: backArrowColor,
-            ),
-          )
-        : const SizedBox(),
-    elevation: 0,
-    title: titleWidget ??
-        Text(
-          title,
-          style: TextStyle(
-              color: titleColor,
-              fontWeight: FontWeight.w600,
-              fontFamily: fontFamily ?? ''),
-        ),
-    backgroundColor: toolbarBackgroundColor,
-    actions: actions,
-    centerTitle: centerTitle,
-    bottom: bottomWidget,
-  );
-}*/
+
 
 class CustomAppbar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
